eedda38fa6fa6114da2c71ae3de3b300d8bc7b0b
TheTytan303 <wciesla303@gmail.com>
2020-02-13 13:45:53 +0100

    OMG

---
+++ app/src/main/java/com/soundbound/outer/API/requestHelper/Expections/ConnectionException.java
@@ -0,0 +1,6 @@
+package com.soundbound.outer.API.requestHelper.Expections;
+
+public class ConnectionException extends Exception {
+    public ConnectionException(){}
+    public ConnectionException(String message){super(message);}
+}
